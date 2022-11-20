ee7c7e98181c47018f291f72e9e9e436275e02fd
Mckiba <wmckiba@gmail.com>
2022-11-20 14:58:38 -0500

    Update Circular loading with drop animation

--- Animations/BallDrop.swift
+++ Animations/BallDrop.swift
@@ -8,81 +8,69 @@
 import SwiftUI
 
 struct BallDrop: View {
-    
-    @State var progress: Double = 0.25
-    
+    @State var progress: CGFloat = 0.25
+    let duration = 2.0
+    let dropDuration = 2.0
+    let circleDelay = 0.60
+    let circleWidth = 150.00
+    @State var isClicked: Bool = false
     var body: some View {
         VStack{
             ZStack{
-                
-                
-                
-                
                 Circle()
-                    .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
-                    .frame(width: 100)
-                    .foregroundStyle(
-                        .white.gradient.shadow(
-                            .inner(color: .gray, radius: 25, x: 25,y: 25)
-                        )
-                    )
+                    .stroke(style: StrokeStyle(lineWidth: 22, lineCap: .round))
+                    .frame(width: 150)
+                    .foregroundStyle(.white.gradient.shadow(.inner(color: .gray, radius: 25, x: 25,y: 25)))
                 
-                //
-                Circle()
-                    .trim(from: 0.25, to: progress)
-                    .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
-                    .frame(width: 100)
-                    .foregroundStyle(
-                        .white.gradient.shadow(
-                            .inner(color: .pink, radius: 25, x: 25,y: 25)
-                        )
-                    )
-                    .animation(.easeOut(duration: 3).repeatForever(autoreverses: false), value: progress)
+                Text(progress.description.prefix(4))
                 
+                CustomCircle(color: Color.purple,trimStart: 0.25, trimEnd: progress, progress: progress)
                 
-                Circle()
-                    .trim(from: 0.25, to: progress)
-                    .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
-                    .frame(width: 100)
-                    .foregroundStyle(
-                        .white.gradient.shadow(
-                            .inner(color: .pink, radius: 25, x: 25,y: 25)
-                        )
-                    ).rotation3DEffect(.degrees(180), axis: (x: 0, y:1 , z: 0))
-
-                    .animation(.easeOut(duration: 3).repeatForever(autoreverses: false), value: progress)
-
-                
-                
+                CustomCircle(color: Color.purple,trimStart: 0.25, trimEnd: progress, progress: progress)
+                    .rotation3DEffect(.degrees(180), axis: (x: 0, y:1 , z: 0))
+                 
+                if progress > 0.725 {
                     Rectangle()
-                    .trim(from: 0, to: 1)
-                    .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round))
-                    .frame(width: 20, height: 100)
-                    .foregroundStyle(
-                        .pink
-                    )
- 
-                    
-                
-            }
+                        .trim(from: 0, to: 0.001)
+                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                        .foregroundStyle(
+                            .purple.opacity(0.8)
+                        )
+                        .frame(width: circleWidth, height: 100)
+                        .rotation3DEffect(.degrees(180), axis: (x: 1, y:1 , z: 0))
+                        .animation(.easeOut ,value: progress)
+                        .offset(y: progress > 0.735 ? circleWidth : 0)
+                        .offset(x: 50.0)
+                }
+            }.padding(.vertical)
             
-            Button(action: {
-                increaseProgress()
-            }, label: {
-                Text("Begin")
-            })
-            
-        }
+            Slider(value: $progress, in: 0.28...0.75)
+        }.onAppear(perform: {
+            repeat {
+                progress += 0.01
+            }while(progress < 0.73)
+        })
     }
-    
-    func increaseProgress(){
-        repeat {
-            progress += 0.01
-            print(progress)
-        }while(progress <= 0.75)
-    }
-    
 }
+
+
+struct CustomCircle : View  {
+    var color: Color
+    var trimStart, trimEnd, progress: CGFloat
+    var body: some View {
+        
+        Circle()
+         .trim(from: trimStart, to: trimEnd)
+            .stroke(style: StrokeStyle(lineWidth: 22, lineCap: .round))
+            .frame(width: 150)
+            .foregroundStyle(
+                .white.gradient.shadow(
+                    .inner(color: color, radius: 25, x: 25,y: 25)))
+            .animation(.easeOut, value: progress)
+    }
+}
+
+
 
 struct BallDrop_Previews: PreviewProvider {
     static var previews: some View {
