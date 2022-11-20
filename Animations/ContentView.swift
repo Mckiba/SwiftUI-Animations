1139a0bb2d3806f7b58eeb54b02593b611af71b1
Mckiba <wmckiba@gmail.com>
2022-11-20 14:57:18 -0500

    Modify Loading Circles Animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
     var body: some View {
         BallDrop()
     }
--- Animations/LoadingCircle.swift
+++ Animations/LoadingCircle.swift
@@ -1,50 +1,57 @@
-//
-//  LoadingCircle.swift
-//  Animations
-//
-//  Created by McKiba Williams on 11/19/22.
-//
+    //
+    //  LoadingCircle.swift
+    //  Animations
+    //
+    //  Created by McKiba Williams on 11/19/22.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct LoadingCircle: View {
-    @State var spin: Bool = false
+    struct LoadingCircle: View {
+        @State var spin: Bool = false
 
-    var body: some View {
-        
-        VStack(spacing: 100){
-            ZStack {
-                Circle()
-                    .stroke(style: StrokeStyle(lineWidth: 15))
-                .frame(width: 100)
-                .foregroundColor(.green)
-                    .opacity(0.4)
-                                
-                Circle ()
-                .trim(from: 0, to: 0.2)
-                .stroke (style: StrokeStyle(lineWidth: 15))
-                .frame(width: 100)
-                .foregroundColor(.green)
-                    .rotationEffect(Angle(degrees: spin ? 90 : 0))
-                .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: spin)
-            }
+        var body: some View {
             
-            Button(action: {
-                spin = true
-            }, label: {
-                Text("Start Spinning")
-                    .font(.title)
-            })
-
+            VStack(spacing: 100){
+                ZStack {
+                    Circle()
+                        .stroke(style: StrokeStyle(lineWidth: 15))
+                    .frame(width: 100)
+                    .foregroundColor(.orange)
+                        .opacity(0.3)
+                                    
+                    Circle ()
+                    .trim(from: 0, to: 0.2)
+                    .stroke (style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                    .frame(width: 100)
+                    .foregroundColor(.orange)
+                        .rotationEffect(Angle(degrees: spin ? 360 : 0))
+                    .animation(.linear(duration: 3).repeatForever(autoreverses: false), value: spin)
+                    
+                    Circle()
+                        .stroke(style: StrokeStyle(lineWidth: 15))
+                    .frame(width: 150)
+                    .foregroundColor(.pink)
+                        .opacity(0.3)
+                    
+                    Circle ()
+                    .trim(from: 0, to: 0.12)
+                    .stroke (style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                    .frame(width: 150)
+                    .foregroundColor(.pink)
+                        .rotationEffect(Angle(degrees: spin ? 360 : 0))
+                        .rotation3DEffect(.degrees(180), axis: (x:1, y: 1, z: 0))
+                    .animation(.linear(duration: 3).repeatForever(autoreverses: false), value: spin)
+                }.onAppear{spin = true}
+            }
         }
     }
-}
 
-struct LoadingCircle_Previews: PreviewProvider {
-    static var previews: some View {
-        LoadingCircle()
+    struct LoadingCircle_Previews: PreviewProvider {
+        static var previews: some View {
+            LoadingCircle().preferredColorScheme(.dark)
+        }
     }
-}
 
 
 
